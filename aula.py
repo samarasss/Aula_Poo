fbaea9874aad11cc49dc47e95314f9681abc54eb
Samara <samara.sampaio@lsd.ufcg.edu.br>
2025-06-04 09:19:34 -0300

    Adiciona explicações do git

--- aula.py
+++ aula.py
@@ -1,3 +1,13 @@
 print("Olá!")
 
 print("Bem-vindo à aula de POO!")
+
+# 1) Ter um repositório conectado com o GitHub
+# 2) Criar ou modificar um arquivo
+# 3) Adicionar o arquivo ao stage
+# 4) git add . / git add <nome_do_arquivo>
+# 5) Fazer o commit
+# 6) git commit -m "Mensagem do commit"
+# 7) Enviar o commit para o repositório remoto
+# 8) git push origin main
+
